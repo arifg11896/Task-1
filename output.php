c6c8f0b03b804251f108cf54225b1357aa392d9c
arifg11896 <arifg11896@gmail.com>
2020-01-15 11:02:15 +0500

    message

---
+++ output.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>GITHUB project</h2>
+    <ul>procedural</ul>
+    <ol>create</ol>
+    <ol>git pull</ol>
+    <ol>git commit -m 'message'</ol>
+    <ol></ol>
+</body>
+</html>
